bf12176b2a1fe2d07d9d92086fed563c5c5e4388
souvik757 <88132246+souvik757@users.noreply.github.com>
2023-12-22 21:49:21 +0530

    question 1

--- RUNDEBUG.java
+++ RUNDEBUG.java
@@ -1,44 +1,7 @@
-/*
-import java.util.List ;
-import java.util.ArrayList ;
-import java.util.Set ;
-import java.util.HashSet ;
-import java.util.Map ;
-import java.util.HashMap  ;
-import java.util.Queue ;
-import java.util.LinkedList ;
-import java.util.Stack ;
- */
-/*
-import java.IO.BufferedReader ;
-import java.IO.InputStreamReader ;
-import java.IO.IOException ;
- */
-/*
-Demo class of functions that to be run :::
- */
 public class RUNDEBUG {
-    public static void main(String[] args) //throws IOException
+    public static void main(String[] args)
     {
-        //Solution0 obj0 = new Solution0() ;
-        //Solution1 obj1 = new Solution1() ;
-        //Solution2 obj2 = new Solution2() ;
-        //Solution3 obj3 = new Solution3() ;
-        //Solution4 obj4 = new Solution4() ;
-        //Solution5 obj5 = new Solution5() ;
-        //Solution6 obj6 = new Solution6() ;
-        //Solution7 obj7 = new Solution7() ;
-        //Solution8 obj8 = new Solution8() ;
-        //Solution9 obj9 = new Solution9() ;
+
     }
 }
-//class Solution0 {}
-//class Solution1 {}
-//class Solution2 {}
-//class Solution3 {}
-//class Solution4 {}
-//class Solution5 {}
-//class Solution6 {}
-//class Solution7 {}
-//class Solution8 {}
-//class Solution9 {}
+
---
+++ TCSNQT_CODING/QUES_1.java
@@ -0,0 +1,68 @@
+package TCSNQT_CODING;
+
+import java.util.Arrays;
+
+/**
+ * Problem Statement –
+
+ A chocolate factory is packing chocolates into the packets.
+ The chocolate packets here represent an array  of N number of integer values.
+ The task is to find the empty packets(0) of chocolate and push it to the end of the conveyor belt(array).
+
+Example 1 :
+
+N=8 and arr = [4,5,0,1,9,0,5,0].
+
+There are 3 empty packets in the given set. These 3 empty packets represented as O should be pushed towards the end of the array
+
+Input :
+
+8  – Value of N
+
+[4,5,0,1,9,0,5,0] – Element of arr[O] to arr[N-1],While input each element is separated by newline
+
+Output:
+
+4 5 1 9 5 0 0 0
+
+Example 2:
+
+Input:
+
+6 — Value of N.
+
+[6,0,1,8,0,2] – Element of arr[0] to arr[N-1], While input each element is separated by newline
+
+Output:
+
+6 1 8 2 0 0
+ */
+public class QUES_1 {
+    public static void main(String[] args) {
+        int[] arr = new int[]{0, 5, 0, 3, 0, 8, 0} ;
+        System.out.println(Arrays.toString(solution_Ques_1.sorted(arr)));
+    }
+}
+class solution_Ques_1 {
+    public static int[] sorted(int[] arr){
+        int s = 0 ;
+        int e = arr.length - 1 ;
+        while (s <= e){
+            if(arr[s] == 0 && arr[e] == 0)
+                e -- ;
+            else if(arr[s] != 0 && arr[e] != 0)
+                s ++ ;
+            else if(arr[s] == 0 && arr[e] != 0)
+            {
+                int temp = arr[s] ;
+                arr[s] = arr[e] ;
+                arr[e] = temp ;
+                s ++ ;
+            }
+            else if(arr[s] != 0 && arr[e] == 0)
+                s ++ ;
+        }
+
+        return arr ;
+    }
+}
